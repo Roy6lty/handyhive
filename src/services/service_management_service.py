ac7e34b22fc9fd27bfd26577aec1d23d4b63bae1
royalty <olowoleru06@gmail.com>
2025-09-10 10:38:52 +0100

    json

--- src/services/service_management_service.py
+++ src/services/service_management_service.py
@@ -3,6 +3,6 @@ from src.models import services_model
 
 
 def get_all_services():
-    with open("services.json", "r") as file:
+    with open("services3.json", "r") as file:
         data = json.load(file)
     return services_model.AllCategory(category=data)
